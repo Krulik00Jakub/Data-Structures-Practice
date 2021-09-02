01b86cec0d6b0d04818569d5d0ffe18f74c87d24
Krulik00Jakub <78824758+Krulik00Jakub@users.noreply.github.com>
2021-09-02 16:03:46 -0400

    Completed first draft of DualArrayDeque.java

--- src/DualArrayDeque.java
+++ src/DualArrayDeque.java
@@ -87,18 +87,58 @@ public class DualArrayDeque<T> extends ArrayList<T> {
 	 */
 	private void balance() {
 		int n = size();
+		
+		/** this part is executed if back is much larger than front */
 		if (3*front.size() < back.size()) {
 			int s = n/2 - front.size();
 			ArrayList<T> l1 = newStack();
 			ArrayList<T> l2 = newStack();
 			
-			/** adds the first half of the elements in DualArrayDeque to l1 */
+			/** adds the first half of the elements in the DualArrayDeque to l1 */
 			for (int i = 0; i < s; i++) {
 				l1.add(i, front.get(i)); //NOT FINISHED
 			}
+			
 			l1.reverse();
 			
+			/** adds all of the elements in the front array to l1 */
+			for (int i = 0; i < front.size(); i++) {
+				l1.add(front.size() + i, front.get(i));
+			}
 			
+			/** adds the remaining elements in back, to l2 */
+			for (int i = s; i < back.size(); i++) {
+				l2.add(i, back.get(i));
+			}
+			
+			front = l1;
+			back = l2;
+			
+		/** this part is executed if front is much larger than back */	
+		} else if (3*back.size() < front.size()) {
+			int s = front.size() - n/2;
+			ArrayList<T> l1 = newStack();
+			ArrayList<T> l2 = newStack();
+			
+			/** adds the second half of front to l1 */
+			for (int i = s; i < front.size(); i++) {
+				l1.add(i, front.get(i));
+			}
+			
+			/** adds the first half of front to l2 */
+			for (int i = 0; i < s; i++) {
+				l2.add(i, front.get(i));
+			}
+			
+			l2.reverse();
+			
+			/** appends back to the end of l2 */
+			for (int i = 0, j = s; i < back.size(); i++, j++) {
+				l2.add(j, back.get(i));
+			}
+			
+			front = l1;
+			back = l2;
 		}
 	}
 
