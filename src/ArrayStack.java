b11c2cf92314313efa1dd19f6dc6de80a4d08624
Krulik00Jakub <78824758+Krulik00Jakub@users.noreply.github.com>
2022-08-12 15:25:22 -0400

    Update ArrayStack.java

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -1,47 +1,46 @@
+
+/**
+ * Implementation of a ArrayList<T> interface using an Array.
+ * All operations take constant time.
+ *
+ * copywright 2021-2022 Jakub Krulik
+ * @author Jakub
+ * 
+ * @param <T> Generic type parameter.
+ */
+
 public class ArrayStack<T> {
-	private T[] a;
-	private int n;
+	T[] a;
+	int n;
 	
 	/**
-	 * Constructs a new array of T with five elements.
+	 * Class constructor for ArrayList<T> interface.
 	 */
 	@SuppressWarnings("unchecked")
 	public ArrayStack() {
-		a = (T[]) new Object[2];
 		n = 0;
-	}
+		a = (T[]) new Object[1];
+	} // end of constructor
 	
 	/**
-	 * Constructs a new array of T with i elements.
-	 * @param i number of elements to initialise the list with
-	 */
-	@SuppressWarnings("unchecked")
-	public ArrayStack(int i) {
-		a = (T[]) new Object[i];
-		n = 0;
-	}
-	
-	/**
-	 * @return n the size of the array
+	 * @return Size of stack
 	 */
 	public int size() {
 		return n;
 	} // end of size() method
 	
 	/**
-	 * Gets the value stored at index i of array a.
-	 * @param i the index of the array
-	 * @return a[i] the value stored
+	 * @param i index of the array
+	 * @return element i in a[]
 	 */
 	public T get(int i) {
 		return a[i];
 	} // end of get(i) method
 	
 	/**
-	 * Sets element i of array a to the value x.
-	 * @param i index of array
-	 * @param x value to be stored
-	 * @return y value previously stored at a[i]
+	 * @param i the index of the array
+	 * @param x the value to be stored at a[i]
+	 * @return previous value of a[i]
 	 */
 	public T set(int i, T x) {
 		T y = a[i];
@@ -49,59 +48,88 @@ public class ArrayStack<T> {
 		return y;
 	} // end of set(i, x) method
 	
+	public void push(T x) {
+		if (n + 1 >= a.length) {
+			resize();
+		}
+		a[n] = x;
+		n++;
+	} // end of push() method
+	
 	/**
-	 * Inserts an element to the index i, moving all elements above
-	 * it up by 1.
-	 * @param i index of array
-	 * @param x value to be stored
+	 * Adds new element to the list
+	 * @param i the index of the array
+	 * @param x the value to be added to a[i]
 	 */
 	public void add(int i, T x) {
-		
-		/*
-		 * If the capacity of the list is 1 less than the number of 
-		 * elements stored, more space is created in the list.
-		 */
 		if (n + 1 > a.length) {
 			resize();
 		}
 		
-		/*
-		 * If the element to be added is smaller than the length of
-		 * the list, elements from i until n are shifted up by 1.
-		 */
-		for (int j = n; j > i; j--) {
+		for (int j = n; j > i; j--) 
 			a[j] = a[j-1];
-		}
-		
 		a[i] = x;
 		n++;
 	} // end of add(i, x) method
 	
+	public void add(T x) {
+		add(0, x);
+	} // end of add(x) method
+	
 	/**
-	 * Removes an element with index i from the array.
-	 * @param i the index of the element to be removed
-	 * @return x the element that was removed
+	 * Removes element from top of the list
+	 * @return value of removed element x
+	 */
+	public T pop() {
+		T x = a[n-1];
+		a[n-1] = null;
+		n--;
+		if (a.length > 3*n) { // used to be >=
+			resize();
+		}
+		return x;
+	} // end of pop() method
+	
+	/**
+	 * Removes element a[i] from the list.
+	 * @param i
+	 * @return element removed from the list
 	 */
 	public T remove(int i) {
 		T x = a[i];
 		for (int j = i; j < n-1; j++) {
 			a[j] = a[j+1];
 		}
-		
-		if (a.length > 3*n) {
-			resize();
-		}
 		n--;
+		if (a.length >= 3*n) resize();
 		return x;
+	} // end of remove(i) method
+	
+	public T remove() {
+		return remove(0);
 	} // end of remove() method
 	
 	/**
-	 * Shrinks the array if the capacity is 3 times larger than the
-	 * size of the stack and grows the array if the capacity is 1
-	 * larger than the size.
+	 * Reverses the contents of the MyArrayList<T>.
+	 */
+	public void reverse() {
+		T x;
+		for (int i = 0, j = n; i < j; i++, j--) {
+			x = a[i];
+			a[i] = a[j];
+			a[j] = x;
+		}
+	} // end of reverse() method
+	
+	/**
+	 * Resizes the list
 	 */
 	@SuppressWarnings("unchecked")
 	private void resize() {
+		
+		/**
+		 * Temporary list for resizing.
+		 */
 		T[] b;
 		
 		if (a.length < n + 1) {
@@ -109,36 +137,66 @@ public class ArrayStack<T> {
 			for (int i = 0; i < n; i++) {
 				b[i] = a[i];
 			}
-		} else if (a.length > 3*n) {
+		} else if (a.length > 2*n) {
 			b = (T[]) new Object[Math.max(a.length/2, 1)];
 			for (int i = 0; i < n; i++) {
 				b[i] = a[i];
 			}
-		} else {
-			return;
+		} else return;
+		a = b;
+	} // end of resize() method
+	
+	/**
+	 * Resizes the list to specific length.
+	 * @param nn the new size of the list
+	 */
+	@SuppressWarnings("unchecked")
+	public void resize(int nn) {
+		T[] b = (T[]) new Object[nn];
+		for (int i = 0; i < n; i++) {
+			b[i] = a[i];
 		}
 		a = b;
 	} // end of resize() method
 	
-	public static void main(String[] args) {
+	/** Clears the list in O(1) time */
+	public String clear() {
+		String x = print();
+		n = 0;
+		resize();
+		return x;
+	} // end of clear() method
+	
+	public String print() {
+		StringBuilder print = new StringBuilder();
+		print.append("[");
+		for (int i = 0; i < n; i++) {
+			print.append(get(i));
+			if (i != n-1) {
+				print.append(", ");
+			}
+		}
+		print.append("]");
+		
+		return print.toString();
+	} // end of print() method
+	
+	/**
+	 * Tests the data structure.
+	 * @param args command line arguments
+	 */
+	public static void main(String args[]) {
+		int m = 100;
 		ArrayStack<Integer> q = new ArrayStack<Integer>();
-		q.add(0, 1);
-		q.add(1, 2);
-		q.add(2, 3);
-		q.add(3, 4);
-		
-		System.out.println(q.get(0));
-		System.out.println(q.get(1));
-		System.out.println(q.get(2));
-		System.out.println(q.get(3));
-		
-		q.add(0, 8);
-		
-		for (int i = q.size(); i > 0; i--) {
-			System.out.println(q.remove(i-1));
+		for (int i = 0; i < m; i++) {
+			q.add(i, i);
 		}
 		
-		System.out.println("\nSize: " + q.size());
-	}
+		System.out.println(q.print());
+		
+		for (int i = 0; i < m; i++) {
+			System.out.println(q.remove(0));
+		}
+	} // end of main() method
 	
-} // end of class ArrayStack<T>
+} // end of class ArrayList
